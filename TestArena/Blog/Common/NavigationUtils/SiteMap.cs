5838cecb24896d294f4f2d17389e10b8198b34c6
Ajay kumar <ajay.kumar@rxglobal.com>
2025-03-30 23:39:20 +0530

    [Blog] [Ajay]: Fix tags

--- TestArena/Blog/Common/NavigationUtils/SiteMap.cs
+++ TestArena/Blog/Common/NavigationUtils/SiteMap.cs
@@ -4,27 +4,58 @@ namespace TestArena.Blog.Common.NavigationUtils
 {
     public static class SiteMap
     {
-        public static List<PageInfo> Pages { get; } = new List<PageInfo>
-        {
-            new("Intro to PACT for .NET Core: API contract testing", "/blog/contract-testing-pact-net-intro"),
-            new("Intro to PACT for .NET Core: Events based systems", "/blog/contract-testing-in-pact-with-events"),
-            new("Intro to PACT for .NET Core: Integration with PactFlow", "/blog/integration-with-pactflow"),
-            new("Integration testing for dotnet core APIs: Introduction", "/blog/integration-testing-in-dotnet-intro"),
-            new("Integration testing for dotnet core APIs: Handling database", "/blog/integration-testing-in-dotnet-with-database"),
-            new("Integration testing for dotnet core APIs: Handling APIs behind authentication", "/blog/integration-testing-in-dotnet-with-auth"),
-            new("Integration testing for dotnet core APIs: Handling 3rd party service calls using wiremock", "/blog/integration-testing-in-dotnet-with-external-services"),
-        };
+        public static List<PageInfo> Pages { get; } =
+        [
+            new("Intro to PACT for .NET Core: API contract testing", 
+            "/blog/contract-testing-pact-net-intro", 
+            new DateTime(2024, 12, 14), 
+            "images/blog/pact/intro/header_landscape.png", 
+            ["PACT", "API"]),
+
+        new("Intro to PACT for .NET Core: Events based systems", 
+            "/blog/contract-testing-in-pact-with-events", 
+            new DateTime(2024, 12, 28), 
+            "images/blog/pact/events-demo/contract-testing-events.webp", 
+            ["PACT", "Events"]),
+
+        new("Intro to PACT for .NET Core: Integration with PactFlow", 
+            "/blog/integration-with-pactflow", 
+            new DateTime(2025, 1, 11), 
+            "images/blog/pact/pact-broker/blog_header.png", 
+            ["PACT", "PactFlow"]),
+
+        new("Integration testing for dotnet core APIs: Introduction", 
+            "/blog/integration-testing-in-dotnet-intro", 
+            new DateTime(2025, 1, 25), 
+            "images/blog/integration-testing/intro/banner.png", 
+            ["Integration Testing", "DotNet"]),
+
+        new("Integration testing for dotnet core APIs: Handling database", 
+            "/blog/integration-testing-in-dotnet-with-database", 
+            new DateTime(2025, 2, 8), 
+            "images/blog/integration-testing/handling-database/banner.png", 
+            ["Integration Testing", "Database"]),
+
+        new("Integration testing for dotnet core APIs: Handling APIs behind authentication", 
+            "/blog/integration-testing-in-dotnet-with-auth", 
+            new DateTime(2025, 2, 22), 
+            "images/blog/integration-testing/handling-auth/banner.png", 
+            ["Integration Testing", "Authentication"]),
+
+        new("Integration testing for dotnet core APIs: Handling 3rd party service calls using wiremock", 
+            "/blog/integration-testing-in-dotnet-with-external-services", 
+            new DateTime(2025, 3, 8), 
+            "images/blog/integration-testing/handling-external-services/banner.png", 
+            ["Integration Testing", "WireMock"])
+        ];
     }
 
-    public class PageInfo
+    public class PageInfo(string header, string relativePath, DateTime publishedOn, string articleImage, List<string> tags)
     {
-        public string Header { get; }
-        public string RelativePath { get; }
-
-        public PageInfo(string header, string relativePath)
-        {
-            Header = header;
-            RelativePath = relativePath;
-        }
+        public string Header { get; } = header;
+        public string RelativePath { get; } = relativePath;
+        public List<string> Tags { get; } = tags;
+        public DateTime PublishedOn { get; } = publishedOn;
+        public string ArticleImage { get; } = articleImage;
     }
 }
