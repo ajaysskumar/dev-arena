b2c0207d533b51d444954e497f9b73f6ba2a9288
Ajay kumar <ajay.kumar@rxglobal.com>
2025-03-30 18:01:53 +0530

    [Blog] [Ajay]: Fix the search links

--- TestArena/Blog/Common/NavigationUtils/SiteMap.cs
+++ TestArena/Blog/Common/NavigationUtils/SiteMap.cs
@@ -8,7 +8,11 @@ namespace TestArena.Blog.Common.NavigationUtils
         {
             new("Intro to PACT for .NET Core: API contract testing", "/blog/contract-testing-pact-net-intro"),
             new("Intro to PACT for .NET Core: Events based systems", "/blog/contract-testing-in-pact-with-events"),
-            new("Intro to PACT for .NET Core: Integration with PactFlow", "/blog/integration-with-pactflow")
+            new("Intro to PACT for .NET Core: Integration with PactFlow", "/blog/integration-with-pactflow"),
+            new("Integration testing for dotnet core APIs: Introduction", "/blog/integration-testing-in-dotnet-intro"),
+            new("Integration testing for dotnet core APIs: Handling database", "/blog/integration-testing-in-dotnet-with-database"),
+            new("Integration testing for dotnet core APIs: Handling APIs behind authentication", "/blog/integration-testing-in-dotnet-with-auth"),
+            new("Integration testing for dotnet core APIs: Handling 3rd party service calls using wiremock", "/blog/integration-testing-in-dotnet-with-external-services"),
         };
     }
 
